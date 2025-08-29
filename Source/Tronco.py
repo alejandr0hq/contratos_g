c8a73abeb1f9b7b4ce4a30514125da580e1914b9
Eduardo Quiroz <al07132544@tecmilenio.mx>
2025-08-29 14:57:20 -0600

    Cree Esqueloto codigo beta

--- Source/Tronco.py
+++ Source/Tronco.py
@@ -1,17 +1,45 @@
-#Este sera el tronco del proyecto 
-b = 123
-a = int(input("Ingresa la contraseña"))
-if b == a :
-    print ("Bienvenido al generador de contratos")
-else:
-    print("Error Intente de nuevo")
-    
-print ("alejandro")
-print ("alejandro")
-print ("alejandro")
+#Generador de contratos
+#Estas son las variables 
+Nombre_Part_de_contrato1 = ""
+Nombre_Part_de_contrato2 = ""
+Dirección = ""
+Fecha = 0
+Cliente = ""
+Costo_de_arrendamientos = ""
+Servicio = ""
+Ubicacion = ""
+Fecha_Incio = ""
+Fecha_Salida = ""
 
-print ("alejandro")
-print ("alejandro")
-print ("alejandro")
+#Este es el menu del generador de contratos
+print("=========== Generador de contratos legales===========: ")
+opcion = ""
+#Aqui entramos al ciclo while
+while opcion != "3": 
+    print("=========\nMenu=========")
+    print("1. Mostrar el contrato de arrendamiento: ")
+    print("2. Mostrar el contrato de prestacion de servicios: ")
+    print("3. Salir del generador de contratos: ")
+#Pedimos Datos
+    opcion = (input("Elija una opcion: "))
 
-    
+    if opcion == "1":
+       Fecha = int(input("Ingrese Fecha para el contrato: "))
+       Nombre_Part_de_contrato1 = (input("Ingrese Nombre del contrato (Arrendamientos): "))
+       Costo_de_arrendamientos = (input("Ingrese la cantidad en pesos: "))
+       Ubicacion = (input("Ingrese la Ubicacion del contrato de arrendamiento: "))
+       Costo_de_arrendamientos = int(input("Ingrese Costo del contrato de arrendamiento: "))
+       Fecha_Incio=  int(input("Ingrese Fecha de Inicio del contrarto: "))
+       Fecha_Salida = int(input("Ingrese Fecha de Final del contrato: "))
+       print("Mostrar Datos del contrato de arrendamientos: ")
+    elif opcion == "2":
+        print("Fin del programa Beta de generador contratos: ")
+
+
+
+
+
+
+
+
+
